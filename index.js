1aa04bd222bab08fef29d9fcf19da420c88ceaf8
Harsh <nidhikumari18878-ctrl.com>
2026-06-07 19:01:52 +0530

    Updated feature

--- index.js
+++ index.js
@@ -266,7 +266,7 @@ app.get("/logout", (req, res) => {
     res.redirect('/login');
 });
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
-});
-
+// app.listen(5000, () => {
+//     console.log("Server is running on port 5000");
+// });
+export default app;
