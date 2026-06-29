47c0a03d5665a2127e3cb3c1f71c4844dcc75e91
Harsh <nidhikumari18878-ctrl.com>
2026-06-29 20:49:26 +0530

    updated code

--- index.js
+++ index.js
@@ -45,9 +45,9 @@ mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("✅ MongoDB connected");
 
-    // app.listen(PORT, () => {
-    //   console.log(`Server running on ${PORT}`);
-    // });
+    app.listen(PORT, () => {
+      console.log(`Server running on ${PORT}`);
+    });
   })
   .catch(err => {
     console.error("❌ MongoDB Error:", err);
@@ -316,12 +316,12 @@ app.get("/logout", (req, res) => {
     res.redirect('/login');
 });
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
-});
-
-// const PORT = process.env.PORT || 5000;
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on ${PORT}`);
+// app.listen(5000, () => {
+//     console.log("Server is running on port 5000");
 // });
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
